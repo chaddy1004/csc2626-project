29a52da461390507014b017da84400cd9cdff2e6
ljazzal <jazzalin.main@gmail.com>
2021-03-12 14:23:25 -0500

    rollout visualizer util

---
+++ data/data_utils.py
@@ -0,0 +1,58 @@
+import os
+import numpy as np
+import pandas as pd
+import random
+import gym
+
+
+def rollout_viz(env, data, seed=None, render=False):
+    """Visualize previously saved rollouts in the gym env"""
+
+    total_reward = 0
+    steps = 0
+    s = env.reset()
+    # env.actionspace.seed(seed)
+    N = data.shape[0]
+    for i in range(N):
+        s = data[i][:8]
+        a = data[i][8:10]
+        r = data[i][10]
+        ns = data[i][11:19]
+        done = data[i][19]
+        # e = data[i][20]
+        env.step(a)
+
+        total_reward += r
+
+        if render:
+            still_open = env.render()
+            if still_open == False: break
+        
+        if steps % 20 == 0 or done:
+            print("observations:", " ".join(["{:+0.2f}".format(x) for x in s]))
+            print("step {} total_reward {:+0.2f}".format(steps, total_reward))
+        steps += 1
+        if done: break
+    return total_reward
+
+
+
+if __name__ == '__main__':
+    # Load rollouts from disk
+    data_dir = os.path.join('LunarLanderData')
+    filename = 'LunarLanderContinuous-v2_none_30.csv'
+    seed = int(filename.split('.')[0][-2:])
+    rollout_name = os.path.join(data_dir, filename)
+
+    # Rollout tuple:
+    # <s0,..,s7,a0,a1,r,s'1,...,s'7,done,episode>
+    rollout = pd.read_csv(rollout_name, header=0).to_numpy() # shape: (N x 21)
+    
+    # Fixing seeds for reproducible behaviour
+    env = gym.make('LunarLanderContinuous-v2')
+    env.seed(seed)
+    env.action_space.seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    
+    rollout_viz(env, rollout, seed=int(seed), render=True)
