8ba7babacd909e429316dee3c596a8e76d8369c9
ljazzal <jazzalin.main@gmail.com>
2021-03-12 14:22:42 -0500

    new dataloader for offline training

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,26 +1,28 @@
-import os
+import os, sys
+import glob
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import torch
-from torch.utils.data import DataLoader, Dataset
-import torchvision.transforms as transforms
-from cv2 import cvtColor, COLOR_BGR2GRAY, resize, INTER_AREA
-from PIL import Image
+from torch.utils.data import DataLoader, Dataset, WeightedRandomSampler
+
+sys.path.append(os.path.abspath(os.path.join('..')))
+from config import Config as cfg
 
 # DATALOADER
-class ExpertDemonstrations(Dataset):
+class OfflineDataset(Dataset):
 
-    def __init__(self, dataframe, root_dir, training=True, transform=None):
-        self.data = dataframe
-        self.root_dir = root_dir
-        self.transform = transform
+    def __init__(self, dataframe, training=True, transform=None):
+        self.data = torch.Tensor(dataframe.to_numpy())
 
-        # Load <s,a,r,s'>
-        self.current_states = self.data['state'].to_list()
-        self.actions = self.data['action'].to_numpy()
-        self.rewards = self.data['reward'].to_numpy()
-        self.next_states = self.data['next'].to_list()
+        # Load <s,a,r,s',done,episode>
+        self.current_states = self.data[:, :8]
+        self.actions = self.data[:, 8:10]
+        self.rewards = self.data[:, 10]
+        self.next_states = self.data[:, 11:19]
+        self.done = self.data[:, 19]
+        self.episode = self.data[:, 20]
+        self.type = self.data[:, -1]
 
     
     def __len__(self):
@@ -28,53 +30,89 @@ class ExpertDemonstrations(Dataset):
 
 
     def __getitem__(self, idx):
-        # Return tuple <s,a,r,s'>
+        # Return tuple <s,a,r,s',done,type>
 
-        current_state = Image.open(os.path.join(self.root_dir, self.current_states[idx]))
-        next_state = Image.open(os.path.join(self.root_dir, self.next_states[idx]))
-        action = self.actions[idx]
-        reward = self.rewards[idx]
-
-        if self.transform:
-            current_state = self.transform(current_state)
-            next_state = self.transform(next_state)
-        
-        return (current_state, action, reward, next_state)
+        return (self.current_states[idx],
+                self.actions[idx],
+                self.rewards[idx],
+                self.next_states[idx],
+                self.done[idx],
+                self.type[idx])
 
 
-class GrayResizeTransform:
-    """Transformation to match pickled data
-        Returns gray (84x84) numpy array
+
+def get_weighted_sampler(ratio, normalize=False):
     """
-    
-    def __call__(self, img):
-        gray = cvtColor(np.array(img), COLOR_BGR2GRAY)
-        resized = resize(gray, (84, 105), interpolation=INTER_AREA)
-        # Crop the score heading and black bar at the bottom of the frame
-        cropped = resized[15:99]
-        return cropped
+    Returns dataframe and WeightedRandomSampler based on desired ratio of sub-optimal to 
+    optimal data rollouts
+        ratio: split tuple of opt to sub-opt data, e.g. (0.1, 0.9) for
+               10% optimal and 90% suboptimal training data
+        normalize: whether the weights should be normalized based on count
+    """
+
+    assert np.sum(ratio) == 1.0, "Ratios should some up to 1.0"
+    files = glob.glob(os.path.join(cfg.DATA_PATH, "*.csv"))
+    weights = torch.Tensor()
+    df = pd.DataFrame()
+    rollout_mapping = {"none": 0, "laggy": 1, "noisy": 2}
+    rollout_count = {"opt": 0, "subopt": 0}
+    for file in files:
+        rollout = pd.read_csv(file, header=0, index_col=None)
+        rollout_type = file.split("_")[1]
+        # Append rollout type column to dataframe
+        rollout['type'] = rollout_mapping[rollout_type]
+
+        if rollout_type == cfg.OPT_LABEL:
+            rollout_count["opt"] += len(rollout)
+            weights = torch.cat((weights, ratio[0]*torch.ones(len(rollout))))
+        else:
+            rollout_count["subopt"] += len(rollout)
+            weights = torch.cat((weights, ratio[1]*torch.ones(len(rollout))))
+
+        df = pd.concat((df, rollout))
+
+    # Balancing based on opt vs subopt data availability
+    if normalize:
+        weights[weights==ratio[0]] /= (rollout_count["opt"])#*ratio[0]
+        weights[weights==ratio[1]] /= (rollout_count["subopt"])#*ratio[1]
+
+    # NOTE: sampeld with replacement by default
+    sampler = WeightedRandomSampler(weights, len(weights))
+    return df, sampler
+
 
 
 if __name__ == "__main__":
     # Test
-    root_data_dir = "./AtariHEADArchives/montezuma_revenge/"
-    demo_data = pd.read_csv("./AtariHEADArchives/mr_demo_data_df.csv")
-
-    # Transforms
-    transform = transforms.Compose([
-        GrayResizeTransform(),
-        # transforms.ToTensor() # uncomment for training
-    ])
+    ratio = (0.3, 0.7)
+    rollouts_df, weighted_sampler = get_weighted_sampler(ratio, normalize=True)
 
     # Dataset
-    dataset = ExpertDemonstrations(
-        dataframe=demo_data,
-        root_dir=root_data_dir,
-        transform=transform,
+    dataset = OfflineDataset(
+        dataframe=rollouts_df,
         training=True
     )
-
     print("Length of dataset: ", len(dataset))
-    plt.imshow(dataset[26146][3])
-    plt.title("Reward {}".format(dataset[26146][2]))
-    plt.show()
+
+    # Dataloader
+    train_loader = DataLoader(
+        dataset,
+        batch_size=cfg.BS,
+        num_workers=cfg.WORKERS,
+        sampler=weighted_sampler
+    )
+
+    # Enumeration shows split between optimal and suboptimal
+    # NOTE: sampling with ratios (1, 0), (0, 1) or (0.5, 0.5) breaks down
+    # instead, I used (0.99, 0.01), (0.01, 0.99) and (0.49, 0.51) as an approximation
+    # TODO: fix this!
+    opt = 0
+    subopt = 0
+    for i, data in enumerate(train_loader):
+        counts = np.unique(data[-1].numpy(), return_counts=True)
+        print("Batch ratio: ", counts[1][0]/np.sum(counts[1]), np.sum(counts[1][1:])/np.sum(counts[1]))
+        opt += counts[1][0]
+        subopt += np.sum(counts[1][1:])
+        if i > 10:
+            break
+    print("Overall ratio: ", opt/(opt+subopt), subopt/(opt+subopt))
