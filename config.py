7c47e117b671e03fbf25663a27df8f25aea6637a
ljazzal <jazzalin.main@gmail.com>
2021-03-12 14:23:04 -0500

    new params

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
 
 class Config:
-    ENV_NAME = 'MontezumaRevengeNoFrameskip-v4'
-    GAME_ID = 'montezuma_revenge'
-    DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/AtariHEADArchives/')
-    DEMO_DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), DATA_PATH, 'mr_demo_data.p')
+    # DATALOADING
+    ENV_NAME = 'LunarLanderContinuous-v2'
+    DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data/LunarLanderData/')
+    OPT_LABEL = 'none'
+    
+    # TRAINING
+    BS = 32
+    WORKERS = 1
