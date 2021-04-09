fbd8107f92c563b305d082330dc4c305d59507bc
chaddy <cpaik1004@gmail.com>
2021-04-09 17:21:30 -0400

    remove pandas from sac_offline

--- sac_offline.py
+++ sac_offline.py
@@ -5,7 +5,6 @@ from collections import namedtuple
 
 import gym
 import numpy as np
-import pandas as pd
 import torch
 from torch.optim import Adam
 from torch.utils.tensorboard import SummaryWriter
